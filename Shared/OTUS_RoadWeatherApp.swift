998deef361f4d4f91c6a1061c1e8967265a025bc
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2022-02-03 19:11:41 +0300

    Initial project created

---
+++ Shared/OTUS_RoadWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_RoadWeatherApp.swift
+//  Shared
+//
+//  Created by Вячеслав Погорельский on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_RoadWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
